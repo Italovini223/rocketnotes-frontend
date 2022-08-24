82e515cd8ca616e5c6d4635c858c3a50e1a36204
italo <italovinicius2018@gmail.com>
2022-08-23 21:48:47 -0300

    ♻️refactor: change the name of App file to Details

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './assets/pages/App'
+import App from './assets/pages/Details'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
