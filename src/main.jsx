432a45e0e03d41e0c0822a43c1225b6bb2bbfc70
italo <italovinicius2018@gmail.com>
2022-08-26 14:44:21 -0300

    ✨feat: add in estilo global classroom

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {ThemeProvider} from 'styled-components'
 
-import {theme} from './styles/theme'
+import GlobalStyle from './styles/global'
+
+import theme from './styles/theme'
 
 import {Details} from './pages/Details'
 
@@ -10,6 +12,7 @@ import {Details} from './pages/Details'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}> 
+      <GlobalStyle />
       <Details />
     </ThemeProvider>
   </React.StrictMode> 
