941ad15ca5f5a9fded5affc5bba3c3bc3d9b915a
italo <italovinicius2018@gmail.com>
2022-08-29 22:09:41 -0300

    ✨ feat: conde in imagem do usuário classroom

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,19 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header(){
   return(
     <Container>
+      <Profile>
+        <img 
+          src="https://github.com/Italovini223.png" 
+          alt="Imagem do usuário" 
+        />
 
+        <div>
+          <span>Bem vindo,</span>
+          <strong>Ítalo Vinícius</strong>
+        </div>
+      </Profile>
     </Container>
   );
 }
