7a2cdf2b73254303fd2cb1fab9ee5e0e520b0bcc
italo <italovinicius2018@gmail.com>
2022-08-30 21:37:53 -0300

    ✨ feat: code in ícones classroom

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -47,3 +47,14 @@ export const Profile = styled.div`
   }
 `;
 
+export const Logout = styled.button`
+  border: none;
+  background: none;
+
+  > svg{
+    font-size: 36px;
+    color: ${({theme}) => theme.COLORS.GRAY_100};
+  }
+`;
+
+
