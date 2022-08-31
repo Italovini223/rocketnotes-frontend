ea2df94cc366cf3c2c94c92f47b708196189de4e
italo <italovinicius2018@gmail.com>
2022-08-30 23:28:56 -0300

    ✨ feat: Code in Componente Tag classroom

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,8 @@ import { Container, Links } from "./styles";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { Section } from "../../components/section";
+import { Tags } from "../../components/Tags";
+
 
 
 export function Details(){
@@ -14,6 +16,10 @@ export function Details(){
           <li><a href="#">https://www.rocketseat.com.br/</a></li>
           <li><a href="#">https://www.rocketseat.com.br/</a></li>
         </Links>
+      </Section>
+      <Section title="Marcadores">
+        <Tags title="express" />
+        <Tags title="Node" />
       </Section>
       <Button title="Voltar"/>
     </Container>
