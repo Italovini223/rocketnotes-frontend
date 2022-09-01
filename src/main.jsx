931470a63f9c8e48c293d93ad7845d94a64e7f1e
italo <italovinicius2018@gmail.com>
2022-08-31 21:49:18 -0300

    ✨ feat: code in Aplicando grid na home classroom

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import GlobalStyle from './styles/global'
 
 import theme from './styles/theme'
 
-import {Details} from './pages/Details'
+import {Home} from './pages/Home'
 
 
 
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}> 
       <GlobalStyle />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode> 
 )
