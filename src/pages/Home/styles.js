e4c007bba880b566f44d71a3972a154c575c0493
italo <italovinicius2018@gmail.com>
2022-09-01 22:27:51 -0300

    ✨ feat: code in Logotipo classroom

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -17,21 +17,36 @@ export const Container = styled.div`
 `;
 export const Brand = styled.div`
   grid-area: brand;
-  background: red;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: ${({theme}) => theme.COLORS.BACKGROUND_700};
+
+  background-color: ${({theme}) => theme.COLORS.BACKGROUND_900};
+  
+  > h1 {
+    font-size: 24px;
+    color: ${({theme}) => theme.COLORS.ORANGE};
+  
+  }
 `;
 export const Menu = styled.ul`
-   grid-area: menu;
-  background: blue;
+  grid-area: menu;
+
 `;
 export const Search = styled.div`
-   grid-area: search;
-  background: violet;
+  grid-area: search;
+
 `;
 export const Content = styled.div`
-   grid-area: content;
-  background: gray;
+  grid-area: content;
+
 `;
 export const NewNote= styled.button`
-   grid-area: newnote;
-  background: orange;
+  grid-area: newnote;
+
 `;
