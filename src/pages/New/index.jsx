72722dd953c506850057b6887b65dd38dcf94782
italo <italovinicius2018@gmail.com>
2022-09-04 04:26:23 -0300

    ✨ feat: code in header da New classroom

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,28 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 
 import {Header} from "../../components/Header"
+import { Input } from "../../components/Input";
 
 export function New(){
   return(
     <Container>
       <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar notas</h1>
+            <a href="/">
+              Voltar
+            </a>
+          </header>
+
+          <Input 
+            placeholder="Titulo"
+            type="text"
+          />
+        </Form>
+      </main>
     </Container>
   )
 }
