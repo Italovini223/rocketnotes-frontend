28b6b101d3d943d92b8229162dc6114b4b131383
italo <italovinicius2018@gmail.com>
2022-09-04 04:03:43 -0300

    ✨feat: code in Profile end avatar do usuário class

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {ThemeProvider} from 'styled-components'
-import { SignUp } from './pages/SingUp'
+import { Profile } from './pages/Profile'
 
 import GlobalStyle from './styles/global'
 
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}> 
       <GlobalStyle />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode> 
 )
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  > header {
+    width: 100%;
+    height: 144px;
+
+    background-color: ${({theme}) => theme.COLORS.BACKGROUND_900};
+
+    display: flex;
+    align-items: center;
+
+    padding: 0 124px;
+
+    svg {
+      font-size: 24px;
+      color: ${({theme}) => theme.COLORS.GRAY_100};
+    }
+  }
+`;
+
+export const Form = styled.form`
+  max-width: 340px;
+
+  margin: 30px auto 0;
+
+  > div:nth-child(4) {
+    margin-top: 24px;
+  }
+`;
+
+export const Avatar = styled.div`
+  width: 186px;
+  height: 186px;
+
+  position: relative;
+  margin: -124px auto 32px;
+
+  > img {
+    width: 186px;
+    height: 186px;
+    border-radius: 50%;
+  }
+
+  > label {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: absolute;
+    bottom: 7px;
+    right: 7px;
+
+    width: 48px;
+    height: 48px;
+
+    border-radius: 50%;
+
+    background-color: ${({theme}) => theme.COLORS.ORANGE};
+
+    cursor: pointer;
+
+    svg {
+      width: 20px;
+      height: 20px;
+
+      color: ${({theme}) => theme.COLORS.BACKGROUND_800};
+    }
+
+    input {
+      display: none;
+    }
+  }
+`;
