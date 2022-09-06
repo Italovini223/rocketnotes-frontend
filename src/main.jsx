a3c3eb1f1208af08ada42846b70c3bff48af6825
italo <italovinicius2018@gmail.com>
2022-09-05 23:42:00 -0300

    ✨ feat: add in Rotas classroom

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {ThemeProvider} from 'styled-components'
-import { New } from './pages/New'
+import { Routes } from './routes'
 
 import GlobalStyle from './styles/global'
 
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}> 
       <GlobalStyle />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode> 
 )
