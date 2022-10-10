1b416dd304e8e22d58dfb8368b72fdfe55a84232
Italovini223 <italovinicius2018@gmail.com>
2022-10-09 21:21:17 -0300

    ✨ feat: code in Hook de autenticação classroom

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,19 +1,19 @@
 import {createContext, useContext} from 'react';
 
-const AuthContext = createContext({});
+const AuthContext= createContext({});
 
 function AuthProvider({children}){
-  return (
-    <AuthContext.Provider value={{email: "italo@123"}}>
+  return(
+    <AuthContext.Provider value={{name: "italo vinicius"}}>
       {children}
     </AuthContext.Provider>
   )
 }
 
 function useAuth() {
- const context =  useContext(AuthContext);
+  const context =  useContext(AuthContext);
 
- return context;
+  return context;
 }
 
-export {AuthProvider, useAuth}
+export {AuthProvider, useAuth};
--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,6 @@ import GlobalStyle from './styles/global'
 import theme from './styles/theme'
 
 
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}> 
