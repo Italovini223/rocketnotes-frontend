f38155e7c43394c8462b93c0fac46bb8f43aab64
Italovini223 <italovinicius2018@gmail.com>
2022-10-16 19:21:47 -0300

    ✨ feat: code in Tag selecionada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -17,6 +17,23 @@ import { api } from "../../services/api";
 
 export function Home(){
   const [tags,setTags] = useState([]);
+  const [tagsSelected,setTagsSelected] = useState([]);
+
+  function handleTagSelected(tagName) {
+
+    const alreadySelected = tagsSelected.includes(tagName);
+    
+    if(alreadySelected) {
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName);
+
+      setTagsSelected(filteredTags);
+    } else {
+      
+      setTagsSelected(prevState => [...prevState, tagName]);
+    }
+
+  }
+
 
   useEffect(() => {
     async function fetchTags(){
@@ -36,12 +53,20 @@ export function Home(){
       <Header />
         
       <Menu>
-        <li><ButtonText title="Todos" isActive/></li>
+        <li>
+          <ButtonText 
+            title="Todos" 
+            isActive={tagsSelected.length === 0}
+            onClick={() => handleTagSelected("all")}
+          />
+        </li>
         {
           tags && tags.map(tag => (
             <li key={tag.id}>
               <ButtonText 
                 title={tag.name}
+                isActive={tagsSelected.includes(tag.name)}
+                onClick={() => handleTagSelected(tag.name)}
               />
             </li>
           ))
